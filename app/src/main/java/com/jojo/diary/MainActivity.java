e06584d24160e04c6269bdfb33f5f874b695f38a
jojoli1024 <41715365+jojoli1024@users.noreply.github.com>
2018-10-31 00:16:34 +0800

    界面主要框架，功能： 功能1：设置topbar的标题 功能2：点击按钮，viewpager滑动到响应页面 功能3：滑动页面，tab滑动到对应的选卡按钮（未实现）

--- app/src/main/java/com/jojo/diary/MainActivity.java
+++ app/src/main/java/com/jojo/diary/MainActivity.java
@@ -1,13 +1,171 @@
 package com.jojo.diary;
 
+import android.graphics.Color;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+//
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener, ViewPager.OnPageChangeListener{
+    private ViewPager myViewPager;
+    //要使用的ViewPager
+    private View view,diary,memo,settings;
+    //ViewPager所包含的页面
+    private List<View> pageList;
+    //ViewPager所包含的页面列表
+    private MyPagerAdapter myPagerAdapter;
+    //适配器
+    private TextView topbar_title;
+    private RadioButton topbar_view;
+    private RadioButton topbar_diary;
+    private RadioButton topbar_memo;
+    private RadioButton topbar_settings;
+
+    private boolean isScrolling = false;
+    // 手指是否在滑动
+
+    private boolean isBackScrolling  = false;
+    // 手指离开后的回弹
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initButton();
+
+        initView();
+    }
+
+    public void initButton(){
+        topbar_title = (TextView)findViewById(R.id.topbar_title);
+//        RadioButton topbar_view = (RadioButton)findViewById(R.id.topbar_view);
+//        RadioButton topbar_diary = (RadioButton)findViewById(R.id.topbar_diary);
+//        RadioButton topbar_memo = (RadioButton)findViewById(R.id.topbar_memo);
+//        RadioButton topbar_settings = (RadioButton)findViewById(R.id.topbar_settings);
+
+        topbar_view = (RadioButton)findViewById(R.id.topbar_view);
+        topbar_diary = (RadioButton)findViewById(R.id.topbar_diary);
+        topbar_memo = (RadioButton)findViewById(R.id.topbar_memo);
+        topbar_settings = (RadioButton)findViewById(R.id.topbar_settings);
+
+
+        topbar_view.setOnClickListener(this);
+        topbar_diary.setOnClickListener(this);
+        topbar_memo.setOnClickListener(this);
+        topbar_settings.setOnClickListener(this);
+    }
+
+    public void initView(){
+//        ViewPager myViewPager;
+//        //要使用的ViewPager
+//        View view,diary,memo,settings;
+//        //ViewPager所包含的页面
+//        List<View> pageList;
+//        //ViewPager所包含的页面列表
+//        MyPagerAdapter myPagerAdapter;
+//        //适配器
+
+        myViewPager= (ViewPager)findViewById(R.id.ViewPager);
+
+        LayoutInflater inflater = getLayoutInflater();
+        view = inflater.inflate(R.layout.view_page,null);
+        diary = inflater.inflate(R.layout.diary_page,null);
+        memo = inflater.inflate(R.layout.memo_page,null);
+        settings = inflater.inflate(R.layout.settings_page,null);
+
+        pageList = new ArrayList<View>();
+        pageList.add(view);
+        pageList.add(diary);
+        pageList.add(memo);
+        pageList.add(settings);
+
+        myPagerAdapter = new MyPagerAdapter(pageList);
+        myViewPager.setAdapter(myPagerAdapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+//        TextView topbar_title = (TextView)findViewById(R.id.topbar_title);
+//        功能1：设置topbar的标题
+//        功能2：点击按钮，viewpager滑动到响应页面
+//        功能3：滑动页面，tab滑动到对应的选卡按钮（未实现）
+        switch (v.getId()){
+            case R.id.topbar_view:
+                topbar_title.setText("浏览");
+                myViewPager.setCurrentItem(0);
+                break;
+            case R.id.topbar_diary:
+                topbar_title.setText("日记");
+                myViewPager.setCurrentItem(1);
+                break;
+            case R.id.topbar_memo:
+                topbar_title.setText("备忘录");
+                myViewPager.setCurrentItem(2);
+                break;
+            case R.id.topbar_settings:
+                topbar_title.setText("设置");
+                myViewPager.setCurrentItem(3);
+                break;
+            default:
+                topbar_title.setText("");
+                myViewPager.setCurrentItem(0);
+                break;
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int i) {
+        switch (i){
+            case 1:
+                isScrolling = true;
+                isBackScrolling = false;
+                break;
+            case 2:
+                isBackScrolling = false;
+                isBackScrolling = true;
+                break;
+            default:
+                isScrolling = false;
+                isBackScrolling = false;
+                break;
+        }
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        switch(position){
+            case 0:
+                topbar_view.callOnClick();
+                topbar_view.setBackgroundColor(Color.BLUE);
+                //上面两个都没有用！！！
+                break;
+            case 1:
+                topbar_diary.callOnClick();
+                topbar_diary.setBackgroundColor(Color.BLUE);
+                break;
+            case 2:
+                topbar_memo.callOnClick();
+                topbar_memo.setBackgroundColor(Color.BLUE);
+                break;
+            case 3:
+                topbar_settings.callOnClick();
+                topbar_settings.setBackgroundColor(Color.BLUE);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onPageScrolled(int i, float v, int i1) {
+
     }
 }
